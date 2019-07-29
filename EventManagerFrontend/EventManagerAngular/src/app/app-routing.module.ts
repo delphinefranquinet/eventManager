7f21bfbcbd52f204c293d3297dfaa775dd8209cd
Zahraa Al-Sudani <zahraobaid1989@gmail.com>
2019-07-29 15:50:46 +0200

    Angular structure

--- EventManagerFrontend/EventManagerAngular/src/app/app-routing.module.ts
+++ EventManagerFrontend/EventManagerAngular/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
